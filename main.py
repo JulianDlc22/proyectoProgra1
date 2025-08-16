76359a2eee09d8d6310958d53f57f29b855e849a
JulianDlc <ja.dlcastillo@gmail.com>
2025-08-16 17:10:25 -0600

    creacion de clase Tienda y clase Gestion

---
+++ main.py
@@ -0,0 +1,64 @@
+class Tienda:
+    def __init__(self, codigo, nombre, categoria, precio, stock):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.categoria = categoria
+        self.precio = precio
+        self.stock = stock
+
+    def VerInfo(self):
+        return f"Codigo: {self.codigo} - Nombre del Producto: {self.nombre} - Categoria: {self.categoria} - Precio: Q.{self.precio} - Stock: {self.stock} unidades "
+
+class GestionTienda:
+    def __init__(self):
+        self.Tienda = {}
+
+    def AgregarProducto(self):
+
+        print("--Sistema de Ingreso de Productos--")
+
+        codigo = input("\nCodigo del Producto: ")
+        if codigo in self.Tienda:
+            input("Codigo ingresado ya en exitencia, presione enter para continuar")
+            return
+
+        nombre = input("Nombre del Producto: ")
+        categoria = input("Categoria del Producto: ")
+
+        while True:
+            try:
+             precio = float(input("Precio del Producto: "))
+             if precio < 0:
+                 input("error- precio no valido, presione enter para continuar ")
+                 continue #vuelve al inicio del while para pedir de nuevo el precio.
+
+             break #se ejecuta si el precio es correcto y te saca del while
+            except ValueError:
+                input("error- precio no valido, presione enter para volver a ingresarlo")
+
+        while True:
+            try:
+             stock = int(input("Stock del Producto: "))
+             if precio <=0:
+                 input("error- stock no valido, presione enter para continuar ")
+                 continue
+
+             break
+            except ValueError:
+                input("error- Stock no valido, presione enter para volver a ingresarlo")
+
+        self.Tienda[codigo] = Tienda(codigo, nombre, categoria, precio, stock)
+        print("Producto Agregado Exitosamente...")
+
+    def ListaProductos(self):
+        pass #Aqui hace tu chance jeanca xdxd
+
+    def BuscarProducto(self):
+        print("")
+
+
+
+
+registro = GestionTienda()
+
+registro.AgregarProducto()
